06c0af6009beaceaeb5ef3fb6a8dd898dc63c071
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-22 05:54:08 -0800

    add TestIntegerMatrix

---
+++ src/homework1/IntegerMatrix.java
@@ -0,0 +1,18 @@
+package homework1;
+
+public class IntegerMatrix extends GenericMatrix<Integer> {
+    @Override
+    protected Integer add(Integer o1, Integer o2) {
+        return 0;
+    }
+
+    @Override
+    protected Integer multiply(Integer o1, Integer o2) {
+        return 0;
+    }
+
+    @Override
+    protected Integer zero() {
+        return 0;
+    }
+}
---
+++ src/homework1/RationalMatrix.java
@@ -0,0 +1,19 @@
+package homework1;
+
+public class RationalMatrix extends GenericMatrix<Rational>{
+
+    @Override
+    protected Rational add(Rational o1, Rational o2) {
+        return null;
+    }
+
+    @Override
+    protected Rational multiply(Rational o1, Rational o2) {
+        return null;
+    }
+
+    @Override
+    protected Rational zero() {
+        return null;
+    }
+}

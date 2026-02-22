5653fc2ee384688a85db9eee663e4c2f351385a1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-22 05:14:26 -0800

    add GenericMethodDemo

---
+++ src/homework1/BoundedTypeDemo.java
@@ -0,0 +1,19 @@
+//package homework1;
+//
+//import java.awt.*;
+//
+//public class BoundedTypeDemo {
+//    public static void main(String[] args) {
+//        Rectangle rectangle = new Rectangle(2, 2);
+//        Circle circle = new Circle(2);
+//
+//        System.out.println("Same area? " +
+//                equalArea(rectangle, circle));
+//    }
+//
+//    private static String equalArea(Rectangle rectangle, Circle circle) {
+//        return rectangle.getArea() == circle.getArea();
+//    }
+//
+//
+//}
